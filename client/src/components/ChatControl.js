6faf2ba26eb5e64eebbd180ca966fdee99a55eeb
flammenoire <rikardo1618@gmail.com>
2018-08-23 03:37:36 +0300

    add message field component

--- client/src/components/ChatControl.js
+++ client/src/components/ChatControl.js
@@ -1,15 +1,27 @@
 import React, {Component} from 'react'
 
 export default class ChatControl extends Component {
+    constructor() {
+        super()
+
+        this.state = {inputMessage: ''}
+
+        this.onMessageChange = (event) => {
+            this.setState({inputMessage: event.target.value})
+        }
+        this.messageSend = () => {
+            ws.emit(this.state.inputMessage)
+        }
+    }
     render() {
         return (
             <div class="chat-message clearfix">
-                <textarea placeholder="Type your message" rows="3"></textarea>
+                <textarea placeholder="Type your message" rows={3} onChange={this.onMessageChange} />
 
-                <i class="fa fa-file-o"></i> &nbsp;&nbsp;&nbsp;
-                <i class="fa fa-file-image-o"></i>
+                <i class="fa fa-file-o" /> &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-file-image-o" />
 
-                <button>Send</button>
+                <button onClick={this.messageSend}>Send</button>
 
             </div>
         )
